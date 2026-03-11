0422c09ad3f370b6a4a123a02b4d47a2250c94fb
ClaudioFernandesGpe <claudiofernandesgpe@gmail.com>
2026-03-10 22:50:41 -0300

    feat: rotas listar tarefas, criar tarefa e buscar tarefa por id adicionadas ao projeto

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from typing import List, Optional
 app = FastAPI(title= "API de Tarefas")
 
 # MODELO DE DADOS
-class tarefa(BaseModel):
+class Tarefa(BaseModel):
     id: Optional[int] = None
     titulo: str
     descricao: str
@@ -13,4 +13,27 @@ class tarefa(BaseModel):
 
 # BANCO EM MEMÓRIA
 db_tarefas = []
+
+# ROTAPARA LISTAR TODAS AS TAREFAS
+@app.get("/tarefas", response_model=List[Tarefa])
+async def  listar_tarefas():
+    return db_tarefas
+
+# ROTA PARA CRIAR TAREFA
+@app.post("/tarefas", response_model=Tarefa, status_code=201)
+async def criar_tarefa(tarefa: Tarefa):
+    tarefa.id = len(db_tarefas) + 1
+    db_tarefas.append(tarefa)
+    return tarefa
+
+# ROTA PARA BUSCAR TAREFA POR ID
+@app.get("/tarefas/{tarefa_id}", response_model=Tarefa)
+async def buscar_tarefa(tarefa_id: int):
+    for tarefa in db_tarefas:
+        if tarefa.id == tarefa_id:
+            return tarefa
+    
+    raise HTTPException(status_code=404, detail="Tarefa não encontrada!")
+
+
 
